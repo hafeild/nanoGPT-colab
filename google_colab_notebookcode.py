4ca3fc7ef8dd3aeea610af694269480110de6959
Hank Feild <hank@feild.org>
2023-10-01 12:29:11 -0400

    Added files to make it easy to run a custom model on Google Colab.

---
+++ google_colab_notebookcode.py
@@ -0,0 +1,22 @@
+# Paste this into a Google Colab notebook cell and run it.
+
+# Install necessary packages.
+!pip install torch numpy transformers datasets tiktoken wandb tqdm
+
+# Clone the this repo.
+!git clone https://github.com/hafeild/nanoGPT-colab.git
+
+# Get and prepare the data for training.
+import sys
+# caution: path[0] is reserved for script path (or '' in REPL)
+sys.path.insert(1, 'nanoGPT-colab/data/google_colab_char')
+import prepare
+
+# Train.
+!cd nanoGPT-colab/ && python train.py config/train_google_colab_char.py \
+    --device=cpu --compile=False --eval_iters=20 --log_interval=1 \
+    --block_size=64 --batch_size=12 --n_layer=4 --n_head=4 \
+    --n_embd=128 --max_iters=2000 --lr_decay_iters=2000 --dropout=0.0
+
+# Generate new text.
+!cd nanoGPT/ && python sample.py --out_dir=out-google-colab-char --device=cpu
